fb8ee3c6b82dd0ab06b2e4314e7cd34b37df145f
Chris Troutner <chris.troutner@gmail.com>
2023-01-07 09:53:02 -0800

    Adding standard

---
+++ nostr-backup.js
@@ -0,0 +1,18 @@
+/*
+  This script connects to a public relay and downloads event data associated
+  with a list of public keys. It then republishes those events to a local
+  nostr relay.
+*/
+
+// Public npm libraries
+
+// Local libraries
+
+async function start () {
+  try {
+    console.log('hello world')
+  } catch (err) {
+    console.error(err)
+  }
+}
+start()
