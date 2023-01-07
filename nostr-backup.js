22ac38955cbc733aeb440cb2015230c8191f15cd
Chris Troutner <chris.troutner@gmail.com>
2023-01-07 11:12:42 -0800

    Successfully backing up events and private messages to local relay

--- nostr-backup.js
+++ nostr-backup.js
@@ -5,7 +5,8 @@
 */
 
 // Public npm libraries
-// import { relayInit } from 'nostr-tools'
+const WebSocket = require('ws');
+const { relayInit } = require('nostr-tools')
 const { RelayPool } = require('nostr')
 
 // Local libraries
@@ -18,20 +19,22 @@ const onsats = 'wss://nostr.onsats.org'
 const relays = [wellOrder, onsats]
 
 // pubkeys - must be in bech32 format
-// const trout = '39f70015a75d9b3edaff4164f84eba7abde8e57ee64b4e50d9e404979d4e378a'
+const trout = '39f70015a75d9b3edaff4164f84eba7abde8e57ee64b4e50d9e404979d4e378a'
 const yunginter = '5fd693e61a7969ecf5c11dbf5ce20aedac1cea71721755b037955994bf6061bb'
 // const pubkeys = [trout]
-const pubkeys = [yunginter]
+const pubkeys = [trout, yunginter]
 
 // Local relay
-// const localRelay = 'ws://localhost:8008'
+const localRelayAddr = 'ws://localhost:8008'
 
 // Config
-const EVENTS_PER_RELAY = 2
+const EVENTS_PER_RELAY = 20
 // END CONSTANTS
 
 async function start () {
   try {
+    const localRelay = await connectToLocalRelay()
+
     const pool = RelayPool(relays)
 
     pool.on('open', relay => {
@@ -45,14 +48,41 @@ async function start () {
     })
 
     pool.on('event', (relay, subId, ev) => {
-      console.log('event: ', ev)
+      // console.log('event: ', ev)
+
+      const pub = localRelay.publish(ev)
+
+      // pub.on('ok', (data) => {
+      //   console.log(`local relay has accepted our event`)
+      // })
+      // pub.on('seen', () => {
+      //   console.log(`we saw the event already?`)
+      // })
+      pub.on('failed', reason => {
+        console.log(`failed to publish to local relay: ${reason}`)
+      })
+
+      console.log(`event ${ev.id} processed.`)
     })
+
+
   } catch (err) {
     console.error(err)
   }
 }
 start()
 
-// async function connectToLocalRelay() {
-//   const localRelay =
-// }
+async function connectToLocalRelay() {
+  const localRelay = relayInit(localRelayAddr, WebSocket)
+  await localRelay.connect()
+
+  localRelay.on('connect', () => {
+    console.log(`connected to ${localRelay.url}`)
+  })
+
+  localRelay.on('error', () => {
+    console.log(`failed to connect to ${localRelay.url}`)
+  })
+
+  return localRelay
+}
